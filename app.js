53f07a84bfea7aa730d08ec1446fbb5da5b7d61a
DJ2513 <djontiveros2728@gmail.com>
2022-10-22 11:45:56 -0500

    cambios

--- app.js
+++ app.js
@@ -10,108 +10,120 @@ app.use(express.static(path.join(__dirname, 'css')));
 app.use(bodyParser.urlencoded())
 
 let current_id = 0
+    const cargarSuperheroes = async()=>{
+        try {
+            const respuesta = await fetch('https://www.superheroapi.com/api.php/5590503564398104')
+                console.log(respuesta);
+            if(respuesta.status===200){
+                const datos = await respuesta.json();
 
+            }    
+        }catch (error) {
+            console.log(error)
+        }
+    };
+cargarSuperheroes();
 
-let data = {
-    "response": "success",
-    "id": "70",
-    "name": "Batman",
-    "powerstats": {
-        "intelligence": "100",
-        "strength": "26",
-        "speed": "27",
-        "durability": "50",
-        "power": "47",
-        "combat": "100"
-    },
-    "biography": {
-        "full-name": "Bruce Wayne",
-        "alter-egos": "No alter egos found.",
-        "aliases": [
-            "Insider",
-            "Matches Malone"
-        ],
-        "place-of-birth": "Crest Hill, Bristol Township; Gotham County",
-        "first-appearance": "Detective Comics #27",
-        "publisher": "DC Comics",
-        "alignment": "good"
-    },
-    "appearance": {
-        "gender": "Male",
-        "race": "Human",
-        "height": [
-            "6'2",
-            "188 cm"
-        ],
-        "weight": [
-            "210 lb",
-            "95 kg"
-        ],
-        "eye-color": "blue",
-        "hair-color": "black"
-    },
-    "work": {
-        "occupation": "Businessman",
-        "base": "Batcave, Stately Wayne Manor, Gotham City; Hall of Justice, Justice League Watchtower"
-    },
-    "connections": {
-        "group-affiliation": "Batman Family, Batman Incorporated, Justice League, Outsiders, Wayne Enterprises, Club of Heroes, formerly White Lantern Corps, Sinestro Corps",
-        "relatives": "Damian Wayne (son), Dick Grayson (adopted son), Tim Drake (adopted son), Jason Todd (adopted son), Cassandra Cain (adopted ward)\nMartha Wayne (mother, deceased), Thomas Wayne (father, deceased), Alfred Pennyworth (former guardian), Roderick Kane (grandfather, deceased), Elizabeth Kane (grandmother, deceased), Nathan Kane (uncle, deceased), Simon Hurt (ancestor), Wayne Family"
-    },
-    "image": {
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
-    }
-}
+    // let data = {
+    //     "response": "success",
+    //     "id": "70",
+    //     "name": "Batman",
+    //     "powerstats": {
+    //         "intelligence": "100",
+    //         "strength": "26",
+    //         "speed": "27",
+    //         "durability": "50",
+    //         "power": "47",
+    //         "combat": "100"
+    //     },
+    //     "biography": {
+    //         "full-name": "Bruce Wayne",
+    //         "alter-egos": "No alter egos found.",
+    //         "aliases": [
+    //             "Insider",
+    //             "Matches Malone"
+    //         ],
+    //         "place-of-birth": "Crest Hill, Bristol Township; Gotham County",
+    //         "first-appearance": "Detective Comics #27",
+    //         "publisher": "DC Comics",
+    //         "alignment": "good"
+    //     },
+    //     "appearance": {
+    //         "gender": "Male",
+    //         "race": "Human",
+    //         "height": [
+    //             "6'2",
+    //             "188 cm"
+    //         ],
+    //         "weight": [
+    //             "210 lb",
+    //             "95 kg"
+    //         ],
+    //         "eye-color": "blue",
+    //         "hair-color": "black"
+    //     },
+    //     "work": {
+    //         "occupation": "Businessman",
+    //         "base": "Batcave, Stately Wayne Manor, Gotham City; Hall of Justice, Justice League Watchtower"
+    //     },
+    //     "connections": {
+    //         "group-affiliation": "Batman Family, Batman Incorporated, Justice League, Outsiders, Wayne Enterprises, Club of Heroes, formerly White Lantern Corps, Sinestro Corps",
+    //         "relatives": "Damian Wayne (son), Dick Grayson (adopted son), Tim Drake (adopted son), Jason Todd (adopted son), Cassandra Cain (adopted ward)\nMartha Wayne (mother, deceased), Thomas Wayne (father, deceased), Alfred Pennyworth (former guardian), Roderick Kane (grandfather, deceased), Elizabeth Kane (grandmother, deceased), Nathan Kane (uncle, deceased), Simon Hurt (ancestor), Wayne Family"
+    //     },
+    //     "image": {
+    //         "url": "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg"
+    //     }
+    // }
 
-let abom = {
-    "response": "success",
-    "id": "1",
-    "name": "A-Bomb",
-    "powerstats": {
-        "intelligence": "38",
-        "strength": "100",
-        "speed": "17",
-        "durability": "80",
-        "power": "24",
-        "combat": "64"
-    },
-    "biography": {
-        "full-name": "Richard Milhouse Jones",
-        "alter-egos": "No alter egos found.",
-        "aliases": [
-            "Rick Jones"
-        ],
-        "place-of-birth": "Scarsdale, Arizona",
-        "first-appearance": "Hulk Vol 2 #2 (April, 2008) (as A-Bomb)",
-        "publisher": "Marvel Comics",
-        "alignment": "good"
-    },
-    "appearance": {
-        "gender": "Male",
-        "race": "Human",
-        "height": [
-            "6'8",
-            "203 cm"
-        ],
-        "weight": [
-            "980 lb",
-            "441 kg"
-        ],
-        "eye-color": "Yellow",
-        "hair-color": "No Hair"
-    },
-    "work": {
-        "occupation": "Musician, adventurer, author; formerly talk show host",
-        "base": "-"
-    },
-    "connections": {
-        "group-affiliation": "Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom",
-        "relatives": "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
-    },
-    "image": {
-        "url": "https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg"
-    }
-}
+    // let abom = {
+    //     "response": "success",
+    //     "id": "1",
+    //     "name": "A-Bomb",
+    //     "powerstats": {
+    //         "intelligence": "38",
+    //         "strength": "100",
+    //         "speed": "17",
+    //         "durability": "80",
+    //         "power": "24",
+    //         "combat": "64"
+    //     },
+    //     "biography": {
+    //         "full-name": "Richard Milhouse Jones",
+    //         "alter-egos": "No alter egos found.",
+    //         "aliases": [
+    //             "Rick Jones"
+    //         ],
+    //         "place-of-birth": "Scarsdale, Arizona",
+    //         "first-appearance": "Hulk Vol 2 #2 (April, 2008) (as A-Bomb)",
+    //         "publisher": "Marvel Comics",
+    //         "alignment": "good"
+    //     },
+    //     "appearance": {
+    //         "gender": "Male",
+    //         "race": "Human",
+    //         "height": [
+    //             "6'8",
+    //             "203 cm"
+    //         ],
+    //         "weight": [
+    //             "980 lb",
+    //             "441 kg"
+    //         ],
+    //         "eye-color": "Yellow",
+    //         "hair-color": "No Hair"
+    //     },
+    //     "work": {
+    //         "occupation": "Musician, adventurer, author; formerly talk show host",
+    //         "base": "-"
+    //     },
+    //     "connections": {
+    //         "group-affiliation": "Hulk Family; Excelsior (sponsor), Avengers (honorary member); formerly partner of the Hulk, Captain America and Captain Marvel; Teen Brigade; ally of Rom",
+    //         "relatives": "Marlo Chandler-Jones (wife); Polly (aunt); Mrs. Chandler (mother-in-law); Keith Chandler, Ray Chandler, three unidentified others (brothers-in-law); unidentified father (deceased); Jackie Shorr (alleged mother; unconfirmed)"
+    //     },
+    //     "image": {
+    //         "url": "https://www.superherodb.com/pictures2/portraits/10/100/10060.jpg"
+    //     }
+    // }
 
 juan = data['biography']
 app.get('/', function(req, res) {
@@ -120,9 +132,9 @@ app.get('/', function(req, res) {
 
 app.post('/', function(req, res) {
     console.log(req.body)
-    res.send("Un post salddvaje")
+    res.send("Un post salvaje")
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on posrt ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
--- css/main.js
+++ css/main.js
@@ -1,26 +1,26 @@
+let superheroe = 1;
 let table = document.getElementById('table')
 let overlay = document.getElementById('card__overlay')
 let card = document.getElementById('card')
 let srbar = document.getElementById('search')
 let lupa = document.getElementById('lupa')
 let srinput = document.getElementById('searchinput')
+const btnSiguiente = document.querySelector('#right')
+const btnAnterior = document.querySelector('#left')
 // let sroverlay = document.getElementById('src__overlay')
-
-overlay.addEventListener("mouseover", function() {
-    card.style.overflowY = "Auto"
-});
-
-overlay.addEventListener("mouseleave", function() {
+const auto = function(){
+    card.style.overflowY = "Auto";
+}
+const hidden = function(){
     card.style.overflowY = "Hidden"
-});
+}
+overlay.addEventListener("mouseover", auto);
 
-table.addEventListener("mouseover", function() {
-    card.style.overflowY = "Auto"
-});
+overlay.addEventListener("mouseleave", hidden);
 
-table.addEventListener("mouseleave", function() {
-    card.style.overflowY = "Hidden"
-});
+table.addEventListener("mouseover", auto);
+
+table.addEventListener("mouseleave", hidden);
 
 srbar.addEventListener("mouseover", function() {
     lupa.style.display = 'none'
@@ -36,11 +36,14 @@ srbar.addEventListener("mouseleave", function() {
     srinput.style.display = 'none'
     lupa.style.display = 'block'
 });
+  // TO-DO Diegordito
+btnSiguiente.addEventListener('click', ()=>{
+    
+})
 
-function regresar() {
-    window.alert("atras")
-
-}
+// function regresar() {
+//     window.alert("atras")
+// }
 
 function siguiente(e) {
     window.alert("siguiente")
